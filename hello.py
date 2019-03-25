2a6a9941d4c09e004d74b2c1ef887ea1c5a13191
Igor Hromov <hromov.igor@gmail.com>
2019-03-26 02:15:31 +0300

    fixing

--- hello.py
+++ hello.py
@@ -129,7 +129,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename + 'uploaded')
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return 'file uploaded!'
+            return 'file uploaded'
     
     return '''
     <!doctype html>
